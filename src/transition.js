ee1c17f59ac2f9c6616774addb0c2d64da3dc932
Tim Roßkamp <23245893+timrosskamp@users.noreply.github.com>
2018-04-16 19:35:28 +0200

    Created src/transition.js

---
+++ src/transition.js
@@ -0,0 +1,32 @@
+export function transition(_options){
+    const options = {
+        from: 0,
+        to: 100,
+        duration: 1000,
+        easing: v => v,
+        onChange: () => {},
+        onDone: () => {}
+    }
+
+    for(const attr in _options){
+        options[attr] = _options[attr];
+    }
+
+    let start = 0;
+
+    const frame = time => {
+        if( start <= 0 ){
+			start = time;
+        }
+
+        const progress = time - start;
+
+        options.onChange( ( options.easing( Math.min( progress / options.duration, 1 ) ) * ( options.to - options.from ) ) + options.from );
+
+        if(progress < options.duration){
+            requestAnimationFrame(frame);
+        }
+    }
+
+    requestAnimationFrame(frame);
+}
